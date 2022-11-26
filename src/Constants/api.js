8e2058753cb3b2bbc412258f86760607736beff8
mirzayevsky <mirzayevsky@gmail.com>
2022-11-27 00:58:54 +0500

    added redux orders

--- src/Constants/api.js
+++ src/Constants/api.js
@@ -1,1 +1,5 @@
-export const BASE_URL = `http://localhost:3004`
+export const BASE_URL = `http://localhost:3004`;
+export const END_COURSES = `courses`;
+export const END_ORDERS = `orders`;
+export const END_USERS = `users`;
+
